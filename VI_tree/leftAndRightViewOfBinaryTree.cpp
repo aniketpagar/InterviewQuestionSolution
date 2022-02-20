610bd08ef85a9bc61915092f07277cdb96a069bf
Aniket Pagar <aniket.dadajipagar@mavenir.com>
2022-02-20 22:50:47 +0530

    prblm

--- VI_tree/leftAndRightViewOfBinaryTree.cpp
+++ VI_tree/leftAndRightViewOfBinaryTree.cpp
@@ -11,7 +11,7 @@ tree -->
 #include "iostream"
 using namespace std;
 #include "queue"
-
+#include "list"
 //Node data structure
 struct Node
 {
@@ -48,9 +48,10 @@ Node* insert( Node *temp,int data)
 }
 
 //function to display left view of tree
+//BFS
 void displayLeftViewOfTree(Node* temp)
 {
-    cout<<" display the left view of binary tree =>"<<endl;
+    cout<<"\ndisplay the left view of binary tree =>"<<endl;
     if(temp != NULL)
     {
         queue<Node*>q1;
@@ -72,10 +73,50 @@ void displayLeftViewOfTree(Node* temp)
     }
 }
 
+//function to display top view of tree
+void displayTopViewOfTree(Node* temp)
+{
+    queue<Node*>q;
+    q.push(temp);
+    list<int>ans;
+    int rootData= temp->data;
+
+    while(!q.empty())
+    {
+        int size = q.size();
+        //storing data according to top view
+        if(q.size()==1)//only single element in tree level.
+        {
+            if(rootData< q.front()->data)
+                ans.push_back(q.front()->data);
+            else
+                ans.push_front(q.front()->data);
+        }
+        else//to store first & last element of level
+        {
+            ans.push_front(q.front()->data);
+            ans.push_back(q.back()->data);
+        }
+
+        for(int i =0;i<size;i++)
+        {
+            Node *d = q.front();
+            q.pop();
+            if(d->left != NULL)
+                q.push(d->left);
+            if(d->right != NULL)
+                q.push(d->right);
+        }
+    }
+    cout<<"\n display the top view of binary tree =>"<<endl;
+    for(auto it: ans)
+        cout<<it<<" ";
+}
+
 //function to display right view of tree
 void displayRightViewOfTree(Node* temp)
 {
-    cout<<" display the right view of binary tree =>"<<endl;
+    cout<<"\ndisplay the right view of binary tree =>"<<endl;
     if(temp != NULL)
     {
         queue<Node*>q1;
@@ -109,5 +150,6 @@ int main()
 
     displayLeftViewOfTree(root);
     displayRightViewOfTree(root);
+    displayTopViewOfTree(root);
 
 }
