cc1c96b78903ab709d62ffb606e20b8b621a8d28
Aniket Pagar <aniket.dadajipagar@mavenir.com>
2022-02-24 10:52:42 +0530

    prblm

---
+++ VIII_cppConceptualQue/abstractClass.cpp
@@ -0,0 +1,30 @@
+/*
+    an abstract class in C++ is a class that has at least one pure virtual function 
+    (i.e., a function that has no definition). The classes inheriting the abstract class
+     must provide a definition for the pure virtual function; 
+*/
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+class base
+{
+    public:
+    base(){}
+    virtual void display()=0;
+};
+class derived : public base
+{
+    public:
+    derived(){}
+    void display()
+    {
+        cout<<"I am in a display function";
+    }
+};
+int main()
+{
+    base *b1= new derived;
+    b1->display();
+
+}
