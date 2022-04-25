3d3a504419a2da01fe09257daffb519a5313d8dc
Aniket Pagar <aniket.dadajipagar@mavenir.com>
2022-04-25 18:47:46 +0530

    prblm

--- VIII_cppConceptualQue/spiralElementInMatrix.cpp
+++ VIII_cppConceptualQue/spiralElementInMatrix.cpp
@@ -2,11 +2,17 @@
 using namespace std;
 
 int main() {
-    vector<vector<int>> vec = { { 1, 2, 3, 4, 5 }, 
-                                { 16, 17, 18, 19, 6 },
-                                { 15, 24, 25, 20, 7 },
-                                { 14, 23, 22, 21, 8}, 
-                                { 13, 12, 11, 10, 9 }
+    // vector<vector<int>> vec = { { 1, 2, 3, 4, 5 }, 
+    //                             { 16, 17, 18, 19, 6 },
+    //                             { 15, 24, 25, 20, 7 },
+    //                             { 14, 23, 22, 21, 8}, 
+    //                             { 13, 12, 11, 10, 9 }
+    //                           };
+    vector<vector<int>> vec = { { 1}, 
+                                {2},
+                                {3},
+                                {4}, 
+                                {5} 
                               };
 
     // The boundaries for the current iteration
@@ -20,11 +26,17 @@ int main() {
         }
         top++;
 
+        if(!(top <= bottom && left <= right))
+         break;
+
         // Print right side 
         for(int i = top; i <= bottom; i++) {
             cout << vec[i][right] << " ";
         }
         right--;
+
+        if(!(top <= bottom && left <= right))
+         break;
 
         // Print Bottom Row
         for(int i = right; i >= left; i--) {
@@ -32,6 +44,8 @@ int main() {
         }
 
         bottom--;
+        if(!(top <= bottom && left <= right))
+         break;
 
         // Print left row
         for(int i = bottom; i >= top; i--) {
