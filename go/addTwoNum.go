fbf65ad99e5fd13e7ed673811c5e642d69003f44
Aniket Pagar <aniket.dadajipagar@mavenir.com>
2022-05-12 10:49:46 +0530

    prblm

--- go/addTwoNum.go
+++ go/addTwoNum.go
@@ -6,7 +6,8 @@ func add(a, b int) int {
 	return a + b
 }
 func main() {
-	var a, b int = 10, 20
+	//var a, b int = 10, 20
+	var a, b = 10, 20
 	var sum int = add(a, b)
 
 	fmt.Print("sum of ", a, " & ", b, " : ", sum)
