3d00964e5429723878010bdb32fc5a3ee8285f6b
Aniket Pagar <aniket.dadajipagar@mavenir.com>
2022-05-20 16:21:38 +0530

    stack implementation in go

---
+++ go/stack.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type stack struct {
+	data []int
+}
+
+var stackSize int = 5
+
+func (s *stack) push(data int) {
+	if len(s.data) >= 4 {
+		fmt.Println(" stack overflow")
+		return
+	}
+	s.data = append(s.data, data)
+}
+
+//pop will remove value from the end, return the value
+func (s *stack) pop() int {
+
+	l := len(s.data) - 1
+	if l < 0 {
+		fmt.Println("stack is empty")
+		return -1
+	}
+	remove := s.data[l]
+	s.data = s.data[:l]
+	return remove
+}
+func main() {
+
+	mystack := stack{}
+	mystack.push(1)
+	mystack.push(2)
+	mystack.push(3)
+	mystack.push(4)
+	mystack.push(5)
+	fmt.Println(mystack)
+	mystack.pop()
+	fmt.Println(mystack)
+	mystack.pop()
+	mystack.pop()
+	mystack.pop()
+	mystack.pop()
+
+}
